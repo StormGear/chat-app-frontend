8d89c96c7f21a71565515baaafacbc2aea69bf14
Papa Kofi Boahen <papakofiboahen@gmail.com>
2025-11-20 17:03:44 +0000

    changing alignment

--- src/components/ConversationMessage.tsx
+++ src/components/ConversationMessage.tsx
@@ -10,7 +10,7 @@ interface ConversationMessageProps {
 
 export const ConversationMessage: React.FC<ConversationMessageProps> = ({textmessage, isCurrentUser, created_at, recipient_username}) => {
     return (
-    <div style={{ textAlign: isCurrentUser ? 'right' : 'left' }} className={`my-5 mx-5`}>
+    <div style={{ textAlign: isCurrentUser ? 'right' : 'left' }} className={`my-5 mx-5 p-5`}>
       <div className="flex flex-col">
         {!isCurrentUser && <b>{recipient_username}</b>}
         {textmessage}
